6cd575c694966300f670896cd2503b584249162e
Amanda Rover <rover.amanda@gmail.com>
2017-10-30 14:40:34 -0200

    Create a new event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,9 +1,12 @@
 class EventsController < ApplicationController
-  protect_from_forgery
+  protect_from_forgery with: :null_session, only: Proc.new { |c| c.request.format.json? }
 
   def new
   end
 
   def create
+    @event = Event.new(params)
+    @event.save
+    redirect_to @event
   end
 end
