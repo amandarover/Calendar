67085ddb4c39b1a68b378dfc45ece889d684c575
Amanda Rover <rover.amanda@gmail.com>
2017-11-22 09:54:27 -0200

    Create stadard CRUD actions in events controller
    Fix error noMethodError in Events#show

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,13 +1,29 @@
 class EventsController < ApplicationController
   protect_from_forgery with: :null_session, only: Proc.new { |c| c.request.format.json? }
 
+  def index
+  end
+
+  def show
+
+  end
+
   def new
+  end
+
+  def edit
   end
 
   def create
     @event = Event.new(event_params)
     @event.save!
     redirect_to @event
+  end
+
+  def update
+  end
+
+  def destroy
   end
 
   private
