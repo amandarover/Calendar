9218db2bfa3995b3bcbf78f9bd6c069dd74a5976
Amanda Rover <rover.amanda@gmail.com>
2017-12-21 13:39:00 -0200

    Implements show method on events_controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,6 +2,7 @@ class EventsController < ApplicationController
   protect_from_forgery with: :null_session, only: Proc.new { |c| c.request.format.json? }
 
   def index
+    @events = Event.all
   end
 
   def show
