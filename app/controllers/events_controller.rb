7a78b1353525a99aaf1bf43e648900fab8e3771b
amanda rover <rover.amanda@gmail.com>
2021-01-15 13:55:00 -0300

    Removes crude methods from events_controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,40 +4,4 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
-
-  def show
-    @event = Event.find params[:id]
-  end
-
-  def new
-    @event = Event.new(event_params)
-  end
-
-  def edit
-    @event = Event.find(params[:id])
-  end
-
-  def create
-    @event = Event.new(event_params)
-    @event.save!
-    redirect_to @event
-  end
-
-  def update
-    @events = Event.find(params[:id])
-
-    if @event.update(event_params)
-      redirect_to @event
-    else
-      render 'edit'
-    end
-  end
-
-  def destroy
-  end
-
-  private
-  def event_params
-    params.permit(:name, :date, :hour, :description, :local, :repeating_frequency)
-  end
 end
