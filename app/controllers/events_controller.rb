7979509557b9686622ae4dc5f10814586b6009a0
amanda rover <rover.amanda@gmail.com>
2021-01-15 14:14:36 -0300

    Creates index method

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,10 @@
 class EventsController < ApplicationController
   protect_from_forgery with: :null_session, only: Proc.new { |c| c.request.format.json? }
 
+  def index
+    @events = Event.all
+  end
+
   def show
     @event = Event.find params[:id]
   end
