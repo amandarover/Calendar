03a00be2c9fe15de13b4bdc318eb96af08d9206a
amanda rover <rover.amanda@gmail.com>
2021-01-15 14:25:29 -0300

    Revert "Creates index method"
    This reverts commit 7979509557b9686622ae4dc5f10814586b6009a0.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,6 @@
 class EventsController < ApplicationController
   protect_from_forgery with: :null_session, only: Proc.new { |c| c.request.format.json? }
 
-  def index
-    @events = Event.all
-  end
-
   def show
     @event = Event.find params[:id]
   end
