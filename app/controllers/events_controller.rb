094bcfc972b582eb53bf8c7e9b1bce753183a5a3
amanda rover <rover.amanda@gmail.com>
2021-01-15 14:25:41 -0300

    Revert "Removes crude methods from events_controller"
    This reverts commit 51d82f02889533b7e68e49718729e286cb5ce884.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,43 @@
 class EventsController < ApplicationController
   protect_from_forgery with: :null_session, only: Proc.new { |c| c.request.format.json? }
 
+  def index
+    @events = Event.all
+  end
+
   def show
     @event = Event.find params[:id]
+  end
+
+  def new
+    @event = Event.new(event_params)
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.save!
+    redirect_to @event
+  end
+
+  def update
+    @events = Event.find(params[:id])
+
+    if @event.update(event_params)
+      redirect_to @event
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+  end
+
+  private
+  def event_params
+    params.permit(:name, :date, :hour, :description, :local, :repeating_frequency)
   end
 end
